b122fd103d060d0894b5014740227bd1a653afe5
hemantparmar-cmd <hemant.parmar@brandcentral.in>
2026-01-21 16:52:03 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Steel Kitchen Container",
+    price: 799,
+    image: "images/p1.jpg"
+  },
+  {
+    id: 2,
+    name: "Plastic Storage Box",
+    price: 499,
+    image: "images/p2.jpg"
+  },
+  {
+    id: 3,
+    name: "Premium Jar Set",
+    price: 1299,
+    image: "images/p3.jpg"
+  }
+];
